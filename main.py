b147c5c7d7953b1ab07e20c6a96c09d166244492
jppacheaco <johnpaulpacheaco@gmail.com>
2022-10-21 11:10:29 -0700

    brainstorming

---
+++ main.py
@@ -0,0 +1,19 @@
+#something along the lines of connectivity
+#ideas include social apps/mental wellness, friendship, dating, mindfulness, music, games, peer-to-peer interaction, special interest groups
+
+#messaging app with large group chats where you are only notified when there are certain keywords of things that you are specifically interested in
+
+#messaging app where there is only a certain number of notifications per time interval
+
+#social media app with tiers of friends lists, get notified for personal friends (both have to agree), tier of people with low followers but arent personal friends, tier with high profile celebrities that wont be pushed as hard
+
+#could be a mindfullness component where if you spend a certain amount of time on it scrolling it tells you to get up and go outside
+
+#built in social media where you can challenge other players to games within the app/ a game that has a whole social media side where it starts with the interaction component and then you can jump into games with other of your friends and posts will be centered around what your friends are doing in that game
+
+#adding a discord bot that can do any of the above within the discord notification system
+
+#small game where when you encounter another player you can chat with them
+
+#voice chat app like discord but you tell it where you grew up, what schools you went to, etc and it can recommend groups old friends are in. maybe links to meta and can do it based off friends you have on meta?
+
