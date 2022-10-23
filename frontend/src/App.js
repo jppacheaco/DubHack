8247a4cda6b8c5d97cecca0b9d88c13253aa2bfd
Avery Le <len22@wwu.edu>
2022-10-23 07:41:00 -0700

    Testing

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {ContactPage} from './Pages/ContactPage';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactPage />
+    </div>
+  );
+}
+
+export default App;
