9ff8e85d5b3ecc8d9c1a1bf0e01504d6f81792b0
jppacheaco <johnpaulpacheaco@gmail.com>
2022-10-23 11:40:21 -0700

    driver in function form

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 #the main where we find responses and 
+from contextlib import nullcontext
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -14,28 +15,25 @@ app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 users_ref = db.collection(u'users')
 docs = users_ref.stream()
+main_user = db.collection(u'users').document(u'user1')
 
-def makeconnections():
-	localfriends = []
-	available = []
+@app.route('/user_info()')
+def user_info():
+    id = 'unknown user'
+    attributes = []
+    for doc in docs:
+        if doc.id == 'user1':
+            id = doc.id
+            attributes = doc.to_dict()
+    return (id, attributes)
 
-	#while there are no friends nearby keep searching every minute
-	while len(localfriends) == 0:
-		localfriends = runRadius.nearby(db)
-		if len(localfriends) == 0:
-			time.sleep(60)
-
-	#access firestore data
-	main_user = db.collection(u'users').document(u'user1')
-
-	response = input("Are you free to hang out?")
-
-	if response == 'no':
-		#change your status to false
+@app.route('/not_free()')
+def not_free():
+    		#change your status to falseprint("ERROR: Invalid entry")
 		main_user.update({
 			u'busy': True
 		})
-		delay = input("How long will you be busy?")
+		delay = busy_user()
 		#find number and unit from delay
 		
 		sorter = ''.join(x for x in delay if x.isdigit())
@@ -49,93 +47,201 @@ def makeconnections():
 			total = number
 		#wait designated time and then restart
 		else:
-			print("ERROR: Invalid entry. We'll ask again. Please enter a number and time unit (ex. 10 min")
+			bad_entry()
 		time.sleep(total)
 		makeconnections()
 
-	elif response == 'yes':
-		#set busy to false indicating not busy
-		main_user.update({u'busy': False})
-	
-		#search through database for other friends with yes
-		list = (db, localfriends)
-		available = findfriends.findFriends(list)
+@app.route('/ask_free')
+def ask_free():
+    response = input("Are you free to hang out?")
+    return response
 
-		#if no one is available
-		if len(available) == 0:
-			print("All of your friends are busy right now")
+@app.route('/busy_user')
+def busy_user():
+	delay = input("How long will you be busy?")
+	return delay
 
-		#if one friend is available
-		elif len(available) == 1:
-			response = input("Would you like to do something with " + str(available[0]) + "?")
-			if response == 'no':
-				#wait 5 minutes to search again
-				time.sleep(5*60)
-				makeconnections()
-			elif response == 'yes':
-				#set users status to yes and compare with friends status
-				main_user.update({u'status': True})
-				#check if available friend is also interested
-				for doc in docs:
-					#check that friends status
-					if doc.id == available[0]:
-						#if they also want to meet up
-						if (doc.to_dict()).get('status'):
-							#link up with those friends
-							print(str(available[0]) + " is also free!")
-						else:
-							print("Sorry, your friend is busy at the moment.")
-							#wait 5 minutes before searching again
-							time.sleep(5*60*60)
-							makeconnections()
-			else:
-				print("ERROR: Invalid response")
-				makeconnections()
+@app.route('/bad_entry')
+def bad_entry():
+    print("ERROR: Invalid entry")
 
-    
-		#if response is greater than one element
-		else:
-			#ask if you want to link up with any of the friends
-			response = input("Would you like to do something with any of the following friends " + str(', '.join(available)) + "?")
-			#if response is yes
-			if response == 'yes':
-				#which friends
-				interested = (input("Who would you like to hang out with from " + str(', '.join(available)) + "?")).split(" ")
-				# print(interested)
-				mutual = []
-				for doc in docs:
-					if doc.id in interested and doc.id in available:
-						main_user.update({u'status': True})
-						if (doc.to_dict()).get('status'):
-							mutual.append(doc.id)
-				#link up with those friends
-				if len(mutual) == 0:
-					print("Sorry, all of your friends are busy right now.")
-				elif len(mutual) == 1:
-					print(mutual[0] + " is free to hang out!")
-				else:
-					i = 1
-					for friend in mutual:
-						if len(mutual) == i:
-							if len(mutual) == 2:
-								print(" and " + mutual[1] + " are free to hang out")
-							else:
-								print(",and " + friend, end = "")
-						else:
-							i += 1
-							print(friend, end = "")
-					print(" are also free!", end = "")
+@app.route('/single_friend')
+def single_friend(name):
+    response = input("Would you like to do something with " + name + "?")
+    return response
 
-			elif response == 'no':
-				#wait a half hour and then re-search
-				time.sleep(30*60*60)
-				makeconnections()
-	
-			else:
-				print("ERROR: Invalid response")
-				makeconnections()
+@app.route('/busy_friend')
+def busy_friend():
+    print("Sorry, your friends are busy right now")
+
+@app.route('/multi_friend')
+def multi_friends(available):
+    response = input("Would you like to do something with any of the following friends " + str(', '.join(available)) + "?")
+    return response
+
+@app.route('/which_friends')
+def which_friends(available):
+    interested = (input("Who would you like to hang out with from " + str(', '.join(available)) + "?")).split(" ")
+    return interested
+
+@app.route('/update_busy')
+def update_busy():
+		main_user.update({
+			u'busy': True
+		})
+  
+@app.route('/available_friends')
+def available_friends(mutual):
+
+	if len(mutual) == 0:
+		busy_friend()
+	#if one friend
+	elif len(mutual) == 1:
+		print(mutual[0] + " is free to hang out!")
 	else:
-		print("ERROR: Invalid response")
-		makeconnections()
+		i = 1
+		for friend in mutual:
+			if len(mutual) == i:
+				if len(mutual) == 2:
+					print(" and " + mutual[1] + " are free to hang out")
+				else:
+					print(",and " + friend, end = "")
+			else:
+				i += 1
+				print(friend, end = "")
+		print(" are also free!", end = "")
+  
+@app.route('/findfriends')
+def get_available(localfriends):
+    list = (db, localfriends)
+    available = findfriends.findFriends(list)
+    return available
 
-makeconnections()
+@app.route('/makeconnections')
+def makeconnections():
+	localfriends = []
+	#while there are no friends nearby keep searching every minute
+	while len(localfriends) == 0:
+		localfriends = runRadius.nearby(db)
+		if len(localfriends) == 0:
+			time.sleep(60)
+   
+	return localfriends
+	#access firestore data
+ 
+@app.route('/fivesleep')
+def fivesleep():
+	time.sleep(5*60)
+
+@app.route('/updateStatusT')
+def updateStatusT():
+    main_user.update({u'status': True})
+    
+@app.route('/docelements')
+def docelements():
+    elements = []
+    for doc in docs:
+        elements.append((doc.id)(doc.get_dict))
+    return elements
+
+@app.route('/getStatus')
+def getStatus(element):
+    val = (element.to_dict()).get('status')
+    return val
+
+@app.route('/updateStatusF')
+def updateStatusF():
+    main_user.update({u'status': False})
+    
+
+@app.route('/match_responses')
+def match_responses(interested):
+	mutual = []
+	for doc in docs:
+		if doc.id in interested and doc.id in available:
+			main_user.update({u'status': True})
+			if (doc.to_dict()).get('status'):
+				mutual.append(doc.id)
+	return mutual
+    
+@app.route('/thirtysleep')
+def thirtysleep():
+    time.sleep(30*60*60)
+    
+
+### MAIN
+localfriends = makeconnections()
+response = ask_free()
+#not free
+if response == 'no':
+	not_free()
+
+#yes free
+elif response == 'yes':
+	#set busy to false indicating not busy
+	update_busy()
+
+	#search through database for other friends with yes
+	available = get_available(localfriends)
+
+	#if no one is available
+	if len(available) == 0:
+		busy_friend()
+
+	#if one friend is available
+	elif len(available) == 1:
+		response = single_friend(available[0])
+			#wait 5 minutes to search again
+		if response == 'no':
+			updateStatusF()
+			fivesleep()
+			#recall function
+		elif response == 'yes':
+			#set users status to yes and compare with friends status
+			updateStatusT()
+			#check if available friend is also interested
+			elementlist = docelements()
+			for item in elementlist:
+				#check that friends status
+				if item[0] == available[0]:
+					#if they also want to meet up
+					if getStatus(item[1]):
+						#link up with those friends
+						available_friends(available[0])
+					else:
+						busy_friend()
+						#wait 5 minutes before searching again
+						fivesleep()
+						#recall function
+		else:
+			bad_entry()
+			#recall function
+
+
+	#if response is greater than one element
+	else:
+		#ask if you want to link up with any of the friends
+		response = multi_friends(available)
+		#if response is yes
+		if response == 'yes':
+			#which friends
+			updateStatusT()
+			interested = which_friends(available)
+			# print(interested)
+			mutual = match_responses(interested)
+			#link up with those friends
+			available_friends(mutual)
+
+		elif response == 'no':
+			#wait a half hour and then re-search
+			updateStatusF()
+			thirtysleep()
+			#recall function
+		else:
+			bad_entry()
+			#recall function
+
+else:
+	bad_entry()
+	#recall function
+
