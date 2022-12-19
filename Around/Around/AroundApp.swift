e983ebd3e54e137ec3d8dbd7044fcd86622a2286
Avery Le <len22@wwu.edu>
2022-12-19 12:42:00 -0600

    empty folder to work with

---
+++ Around/Around/AroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundApp.swift
+//  Around
+//
+//  Created by Avery Le on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct AroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
