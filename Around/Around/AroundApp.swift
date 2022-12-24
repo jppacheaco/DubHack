af0c3390243c246b03de52f792b80fb26126ff0a
Avery Le <len22@wwu.edu>
2022-12-24 12:40:09 -0600

    Please work

--- Around/Around/AroundApp.swift
+++ Around/Around/AroundApp.swift
@@ -1,12 +1,7 @@
 //
 //  AroundApp.swift
 //  Around
-//
-<<<<<<< HEAD
-//  Created by Avery Le on 12/24/22.
-=======
 //  Created by JP Pacheaco on 12/24/22.
->>>>>>> 2689bbc7b6a527bedb0697f9073f16cae38c9ac5
 //
 
 import SwiftUI
